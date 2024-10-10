274c2fe4dcb8849f65602bf5929ffc859deb362b
Mohamed Raslan <m.raslan97@gmail.com>
2024-10-10 21:20:17 +0300

    first commit

---
+++ lib/login_screen.dart
@@ -0,0 +1,215 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController PassController = TextEditingController();
+
+  bool hiddenPassword = true;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: double.infinity,
+              height: 300,
+              decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("lib/assets/background2.jpg"),
+                      fit: BoxFit.cover)),
+              child: const Padding(
+                padding: EdgeInsets.only(left: 30, top: 75, bottom: 20),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Sign in to your account",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 50,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      Text(
+                        "Sign in to your account",
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 20,
+                        ),
+                      )
+                    ]),
+              ),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              child: TextField(
+                controller: emailController,
+                decoration: InputDecoration(
+                  label: const Text('Email'),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              child: TextField(
+                controller: PassController,
+                obscureText: hiddenPassword,
+                decoration: InputDecoration(
+                  suffix: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        hiddenPassword = !hiddenPassword;
+                      });
+                    },
+                    child: Icon(hiddenPassword
+                        ? Icons.visibility
+                        : Icons.visibility_off),
+                  ),
+                  label: const Text('Password'),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.centerRight,
+              child: TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "Forget password?",
+                    style: TextStyle(color: Colors.blue),
+                  )),
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: CupertinoButton(
+                color: const Color.fromARGB(255, 46, 85, 116),
+                child: const Text("Login"),
+                onPressed: () {},
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(
+                    child: Divider(
+                      thickness: 1,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  Text("or log in with"),
+                  Expanded(
+                    child: Divider(
+                      thickness: 1,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                GestureDetector(
+                  onTap: () {},
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 12, horizontal: 30),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(color: Colors.grey),
+                    ),
+                    child: Row(
+                      children: [
+                        Image.asset(
+                          "lib/assets/google_logo.png",
+                          height: 20,
+                          width: 20,
+                        ),
+                        const Text("Google"),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                GestureDetector(
+                  onTap: () {},
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 12, horizontal: 30),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(color: Colors.grey),
+                    ),
+                    child: Row(
+                      children: [
+                        Image.asset(
+                          "lib/assets/facebook_logo.png",
+                          height: 20,
+                          width: 20,
+                        ),
+                        const Text("Facebook"),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 70,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text("Don't have an account?"),
+                const SizedBox(
+                  width: 4,
+                ),
+                GestureDetector(
+                  onTap: () {},
+                  child: const Text(
+                    "Register",
+                    style: TextStyle(
+                      color: Colors.blue,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
